c296c5a11024962ad2f823bbb1fdf124285886b3
Sagar <53867930+the-e3n@users.noreply.github.com>
2022-07-24 16:00:19 +0530

    REFACTOR: Change key names in config and update readme (#14)

--- constants/settings.go
+++ constants/settings.go
@@ -3,7 +3,7 @@ package constants
 const (
 	DEFAULT_FILE_EXTENSION    = ".sql"
 	SETTINGS_CONFIG_FILE_NAME = ".splinter.yaml"
-	SPLINTER_KEY_PREFIX       = "SPLINTER_"
+	SPLINTER_KEY_PREFIX       = ""
 	DEFAULT_USER_CONFIG_FILE  = "splinter.yaml"
 	DEFAULT_MIGRATION_PATH    = "./migrations"
 )
