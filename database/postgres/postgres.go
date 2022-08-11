fb0c9eecccc36f74cd0ca6d5f7155eb6ac3fc819
Pranjal Verma <42290488+pranjal-verma@users.noreply.github.com>
2022-08-12 00:20:46 +0530

    Refactor/driver (#20)
    * ADD: Close() in driver
    
    * ADD: implment GetSchemaMigrations & UpdateSchemaMigratoins
    
    * WIP: replace runner with migrate
    
    * ADD: method to close,lock and unlock from Migrate
    
    * ADD: method to close connection
    
    * FIX: order of migration file execution
    
    * ADD: Release lock method in migrate

--- database/postgres/postgres.go
+++ database/postgres/postgres.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"sort"
 	"strings"
 
 	_ "github.com/lib/pq"
@@ -89,22 +88,113 @@ func (p *Postgres) Initialize(ctx context.Context) error {
 	return nil
 }
 
-func (p *Postgres) CrossCheckMigrations(ctx context.Context, migrationFiles []string) ([]string, error) {
-	// read from schema_migrations
-	// case 1 migration file does not exist in the table, then execute those migrations
-	// case 2 migration exists in database but does not exist in file system, then throw an error and mark the migration as dirty.
+// func (p *Postgres) CrossCheckMigrations(ctx context.Context, migrationFiles []string) ([]string, error) {
+// 	// read from schema_migrations
+// 	// case 1 migration file does not exist in the table, then execute those migrations
+// 	// case 2 migration exists in database but does not exist in file system, then throw an error and mark the migration as dirty.
 
-	query := getMigrations()
-	isNewMigration := map[string]bool{}
-	maxBatchNumner := 0
-	migrations := []schemaMigration{}
-	latestMigrationFile := []string{}
-	firstIteration := true
-	newMigrations := []string{}
+// 	query := getMigrations()
+// 	isNewMigration := map[string]bool{}
+// 	maxBatchNumner := 0
+// 	migrations := []schemaMigration{}
+// 	latestMigrationFile := []string{}
+// 	firstIteration := true
+// 	newMigrations := []string{}
 
-	for i := range migrationFiles {
-		isNewMigration[migrationFiles[i]] = true
+// 	for i := range migrationFiles {
+// 		isNewMigration[migrationFiles[i]] = true
+// 	}
+// 	sqlRows, err := p.db.Query(query)
+// 	if err != nil {
+// 		logger.Log.WithError(err)
+// 		return nil, err
+// 	}
+
+// 	for sqlRows.Next() {
+// 		var (
+// 			id            int64
+// 			migrationName string
+// 			batchNumber   int
+// 			createdAt     int64
+// 		)
+
+// 		if err = sqlRows.Scan(&id, &migrationName, &batchNumber, &createdAt); err != nil {
+// 			logger.Log.WithError(err)
+// 			return nil, err
+// 		}
+
+// 		if batchNumber > maxBatchNumner {
+// 			maxBatchNumner = batchNumber
+// 		}
+// 		mig := schemaMigration{
+// 			migrationName: migrationName,
+// 			id:            id,
+// 			createdAt:     createdAt,
+// 			batchNumber:   batchNumber,
+// 		}
+// 		migrations = append(migrations, mig)
+
+// 		if firstIteration {
+// 			firstIteration = false
+// 			latestMigrationFile = append(latestMigrationFile, mig.migrationName)
+// 		}
+
+// 	}
+// 	p.latestBatchNumber = maxBatchNumner
+
+// 	for i := range migrations {
+// 		migrationFromDB := migrations[i]
+// 		if _, ok := isNewMigration[migrationFromDB.migrationName]; !ok {
+// 			return nil, errors.New("some migration files  missing in your migration path")
+// 		}
+// 		isNewMigration[migrationFromDB.migrationName] = false
+// 	}
+
+// 	for fileName, isNew := range isNewMigration {
+// 		if isNew {
+// 			newMigrations = append(newMigrations, fileName)
+// 		}
+// 	}
+
+// 	if p.migrationType == constants.MIGRATION_DOWN {
+// 		return latestMigrationFile, nil
+// 	}
+
+// 	sort.Slice(newMigrations, func(i, j int) bool {
+// 		return newMigrations[j] > newMigrations[i]
+
+// 	})
+// 	return newMigrations, nil
+// }
+
+// TODO implment CrossCheckMigrations
+func (p *Postgres) CrossCheckMigrations(ctx context.Context, migrations []string, schemaMigrations []database.SchemaMigration) error {
+	if len(migrations) < len(schemaMigrations) {
+		return errors.New("missing migration files")
 	}
+
+	migrationsFilesMap := map[string]bool{}
+	for i := range migrations {
+		migrationsFilesMap[migrations[i]] = true
+	}
+
+	// order of files is guaranteed to be in sorted order
+	for i := range schemaMigrations {
+		if _, ok := migrationsFilesMap[schemaMigrations[i].MigrationName]; !ok {
+			return fmt.Errorf("missing migration file %v", schemaMigrations[i].MigrationName)
+		}
+	}
+
+	return nil
+}
+
+func (p *Postgres) Close() error {
+	return p.db.Close()
+}
+
+func (p *Postgres) GetSchemaMigrations() ([]database.SchemaMigration, error) {
+	query := getMigrations()
+	result := []database.SchemaMigration{}
 	sqlRows, err := p.db.Query(query)
 	if err != nil {
 		logger.Log.WithError(err)
@@ -124,48 +214,56 @@ func (p *Postgres) CrossCheckMigrations(ctx context.Context, migrationFiles []st
 			return nil, err
 		}
 
-		if batchNumber > maxBatchNumner {
-			maxBatchNumner = batchNumber
+		mig := database.SchemaMigration{
+			MigrationName: migrationName,
+			ID:            id,
+			CreatedAt:     createdAt,
+			BatchNumber:   batchNumber,
 		}
-		mig := schemaMigration{
-			migrationName: migrationName,
-			id:            id,
-			createdAt:     createdAt,
-			batchNumber:   batchNumber,
-		}
-		migrations = append(migrations, mig)
-
-		if firstIteration {
-			firstIteration = false
-			latestMigrationFile = append(latestMigrationFile, mig.migrationName)
-		}
+		result = append(result, mig)
 
 	}
-	p.latestBatchNumber = maxBatchNumner
 
-	for i := range migrations {
-		migrationFromDB := migrations[i]
-		if _, ok := isNewMigration[migrationFromDB.migrationName]; !ok {
-			return nil, errors.New("some migration files  missing in your migration path")
+	return result, nil
+
+}
+
+func (p *Postgres) UpdateSchemaMigrations(migrations []database.SchemaMigration, migrationType string) error {
+	debug := "error updaing schema migrations"
+
+	if migrationType == constants.MIGRATION_UP {
+		query := updateSchemaMigrations(migrations)
+		err := p.execQuery(query)
+		if err != nil {
+			logger.Log.WithError(err).Error(debug)
+			return err
 		}
-		isNewMigration[migrationFromDB.migrationName] = false
+		return nil
 	}
 
-	for fileName, isNew := range isNewMigration {
-		if isNew {
-			newMigrations = append(newMigrations, fileName)
+	if migrationType == constants.MIGRATION_DOWN {
+		query := deleteFromSchemaMigrations(migrations)
+		logger.Log.Info("query ", query)
+		err := p.execQuery(query)
+		if err != nil {
+			logger.Log.WithError(err).Error(debug)
+			return err
 		}
+		return nil
 	}
 
-	if p.migrationType == constants.MIGRATION_DOWN {
-		return latestMigrationFile, nil
+	return errors.New("invalid migrtion type")
+}
+
+func (p *Postgres) execQuery(q string) error {
+	_, err := p.db.Exec(q)
+	if err != nil {
+		logger.Log.Error(err)
+		return err
 	}
 
-	sort.Slice(newMigrations, func(i, j int) bool {
-		return newMigrations[j] > newMigrations[i]
+	return nil
 
-	})
-	return newMigrations, nil
 }
 
 // runs given set of SQL
@@ -209,16 +307,7 @@ func (p *Postgres) Migrate(ctx context.Context, migrationFiles []string) error {
 
 	}
 
-	// updating schema migrations
-	_, err = transaction.Exec(p.updateSchemaMigrations(migrationFiles))
-	if err != nil {
-		logger.Log.WithError(err)
-		transaction.Rollback()
-		return err
-	}
-
 	logger.Log.Info("Commiting transaction")
-
 	err = transaction.Commit()
 	if err != nil {
 		logger.Log.WithError(err)
@@ -351,4 +440,14 @@ func NewPostgresDB(connectionURL, migrationType string) (database.Driver, error)
 	driver.migrationType = migrationType
 
 	return &driver, nil
+}
+
+func NewPostgresDriver(connectionURL string, migrationType string) (database.Driver, error) {
+	db, err := sql.Open("postgres", connectionURL)
+
+	if err != nil {
+		logger.Log.WithError(err)
+		return nil, err
+	}
+	return &Postgres{db: db, migrationType: migrationType}, nil
 }
---
+++ migrate/migrate.go
@@ -0,0 +1,203 @@
+package migrate
+
+import (
+	"context"
+	"errors"
+	"log"
+	"sort"
+	"time"
+
+	"github.com/squareboat/splinter/config"
+	"github.com/squareboat/splinter/constants"
+	"github.com/squareboat/splinter/database"
+	"github.com/squareboat/splinter/database/postgres"
+	"github.com/squareboat/splinter/logger"
+	"github.com/squareboat/splinter/parser"
+)
+
+type Migrate struct {
+	driver database.Driver
+	dbUri  string
+}
+
+func NewMigrate(migrationType string) (*Migrate, error) {
+	driver, err := postgres.NewPostgresDriver(config.GetDbUri(), migrationType)
+	if err != nil {
+		logger.Log.Error(err)
+		log.Fatal(err)
+	}
+	migrate := Migrate{
+		driver: driver,
+		dbUri:  config.GetDbUri(),
+	}
+	return &migrate, nil
+}
+
+func (m *Migrate) Up() error {
+	err := m.driver.Initialize(context.Background())
+	if err != nil {
+		logger.Log.WithError(err)
+		return err
+	}
+	err = m.lock()
+	if err != nil {
+		logger.Log.WithError(err)
+		return err
+	}
+
+	defer func() {
+		m.unlock()
+		m.close()
+	}()
+
+	migrations, err := m.driver.GetSchemaMigrations()
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+
+	migrationFiles, err := parser.GetMigrationFileNames()
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+	err = m.driver.CrossCheckMigrations(context.Background(), migrationFiles, migrations)
+	if err != nil {
+		logger.Log.Warn("error cross checking migrations")
+		return err
+	}
+	logger.Log.Info("cross check success")
+	if len(migrationFiles) == len(migrations) {
+		logger.Log.Warn("no new migrations found")
+		return nil
+	}
+
+	newMigrations := getNewMigrations(migrationFiles, migrations)
+	logger.Log.Info("Migrations  ", newMigrations)
+
+	//migrationFiles[len(migrations):]
+	err = m.driver.Migrate(context.Background(), newMigrations)
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+
+	lastBatchNumber := 1
+	if len(migrations) > 0 {
+		lastBatchNumber = migrations[len(migrations)-1].BatchNumber + 1
+	}
+
+	newSchemaMigrations := []database.SchemaMigration{}
+	for i := range newMigrations {
+		newSchemaMigrations = append(newSchemaMigrations, database.SchemaMigration{
+			MigrationName: newMigrations[i],
+			BatchNumber:   lastBatchNumber,
+			CreatedAt:     time.Now().Unix(),
+		})
+	}
+
+	// insert into schema_migations
+	err = m.driver.UpdateSchemaMigrations(newSchemaMigrations, constants.MIGRATION_UP)
+	if err != nil {
+		logger.Log.WithError(err).Error("error updating schema_migrations")
+		return err
+	}
+	return nil
+}
+
+func (m *Migrate) Down() error {
+	err := m.driver.Initialize(context.Background())
+	if err != nil {
+		logger.Log.WithError(err)
+		return err
+	}
+	err = m.lock()
+	if err != nil {
+		logger.Log.WithError(err)
+		return err
+	}
+
+	defer func() {
+		m.unlock()
+		m.close()
+	}()
+
+	migrations, err := m.driver.GetSchemaMigrations()
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+
+	migrationFiles, err := parser.GetMigrationFileNames()
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+
+	err = m.driver.CrossCheckMigrations(context.Background(), migrationFiles, migrations)
+	if err != nil {
+		logger.Log.Warn("error cross checking migrations")
+		return err
+	}
+	if len(migrations) == 0 {
+		return errors.New("no migrations found")
+	}
+
+	rollbackMigrations := getRollbacks(1, migrations)
+
+	err = m.driver.Migrate(context.Background(), rollbackMigrations)
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+	schemaMigrations := []database.SchemaMigration{}
+	for i := range rollbackMigrations {
+		schemaMigrations = append(schemaMigrations, database.SchemaMigration{MigrationName: rollbackMigrations[i]})
+	}
+	err = m.driver.UpdateSchemaMigrations(schemaMigrations, constants.MIGRATION_DOWN)
+	if err != nil {
+		logger.Log.Error(err)
+		return err
+	}
+
+	return nil
+}
+
+func (m *Migrate) lock() error {
+	return m.driver.Lock()
+}
+
+func (m *Migrate) unlock() error {
+	return m.driver.Unlock()
+}
+
+func (m *Migrate) close() error {
+	return m.driver.Close()
+}
+
+func getNewMigrations(migrationFiles []string, schemaMigrations []database.SchemaMigration) []string {
+	newMigrations := []string{}
+	migrationsFilesMap := map[string]bool{}
+	for i := range schemaMigrations {
+		migrationsFilesMap[schemaMigrations[i].MigrationName] = true
+	}
+
+	// order of files is guaranteed to be in sorted order
+	for i := range migrationFiles {
+		if _, ok := migrationsFilesMap[migrationFiles[i]]; !ok {
+			newMigrations = append(newMigrations, migrationFiles[i])
+		}
+	}
+	sort.Slice(newMigrations, func(i, j int) bool {
+		return newMigrations[i] < newMigrations[j]
+	})
+	return newMigrations
+}
+
+func getRollbacks(count int, schemaMigrations []database.SchemaMigration) []string {
+	return []string{schemaMigrations[len(schemaMigrations)-1].MigrationName}
+}
+
+func (m *Migrate) Release() error {
+	return m.driver.Unlock()
+}
